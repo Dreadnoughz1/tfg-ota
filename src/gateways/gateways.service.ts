37fe5502080e2f62f2aef8331897f21d7b7e26c1
Joan Llop <p.desarrollo.ipla@gmail.com>
2025-12-09 13:55:47 +0100

    gateway service terminado

--- src/gateways/gateways.service.ts
+++ src/gateways/gateways.service.ts
@@ -1,4 +1,91 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { SearchDto } from 'src/shared/dto';
+import { Repository } from 'typeorm';
+import { Gateway } from './entities/gateway.entity';
+import {
+  CreateGatewayDto,
+  PaginatedGatewayResponseDto,
+  UpdateGatewayDto,
+} from './dto';
 
 @Injectable()
-export class GatewaysService {}
+export class GatewaysService {
+  constructor(
+    @InjectRepository(Gateway)
+    private readonly gatewayRepository: Repository<Gateway>,
+  ) {}
+  async create(createGatewayDto: CreateGatewayDto) {
+    const gateway = this.gatewayRepository.create(createGatewayDto);
+
+    await this.gatewayRepository.save(gateway);
+
+    return gateway;
+  }
+
+  async findAll(
+    paginationDto: SearchDto,
+  ): Promise<PaginatedGatewayResponseDto> {
+    const { orderBy, order, page, limit, searchText } = paginationDto;
+
+    const queryBuilder = this.gatewayRepository.createQueryBuilder('gateway');
+
+    if (searchText) {
+      queryBuilder.andWhere('gateway.name ILIKE :search', {
+        search: `%${searchText}%`,
+      });
+    }
+
+    const validOrderFields = ['name', 'id'];
+
+    const safeOrderBy = validOrderFields.includes(orderBy) ? orderBy : 'name';
+
+    queryBuilder.orderBy(`gateway.${safeOrderBy}`, order);
+
+    queryBuilder.skip((page - 1) * limit).take(limit);
+
+    const [data, total] = await queryBuilder.getManyAndCount();
+
+    return {
+      gateways: data,
+      total: total,
+    };
+  }
+
+  async findOne(id: number) {
+    const gateway = await this.gatewayRepository.findOne({
+      where: { id },
+    });
+
+    if (!gateway) {
+      throw new NotFoundException(`Gateway con ID ${id} no encontrado.`);
+    }
+
+    return gateway;
+  }
+
+  async update(id: number, updateGatewayDto: UpdateGatewayDto) {
+    const gateway = await this.gatewayRepository.findOne({
+      where: { id: id },
+    });
+
+    if (!gateway) {
+      throw new NotFoundException(`Gateway con ID ${id} no encontrado.`);
+    }
+    Object.assign(gateway, updateGatewayDto);
+
+    await this.gatewayRepository.save(gateway);
+
+    return gateway;
+  }
+
+  async remove(id: number) {
+    const gateway = await this.gatewayRepository.findOne({
+      where: { id },
+    });
+    if (!gateway)
+      throw new NotFoundException(`Gateway con ID ${id} no encontrado.`);
+
+    await this.gatewayRepository.delete(id);
+  }
+}
