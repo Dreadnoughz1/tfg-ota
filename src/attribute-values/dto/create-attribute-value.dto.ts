d5ad49a758f36e05cdc2efe4d9b7f885a30ba67f
Joan Llop <p.desarrollo.ipla@gmail.com>
2025-12-10 11:38:35 +0100

    validators (no implementados)

--- src/attribute-values/dto/create-attribute-value.dto.ts
+++ src/attribute-values/dto/create-attribute-value.dto.ts
@@ -1,9 +1,6 @@
-import { Machine } from 'src/machines/entities/machine.entity';
-
 export class CreateAttributeValueDto {
-  id: number;
   attributeName: string;
   value: number;
-  timestamp: Date;
-  machine: Machine;
+  timestamp: string;
+  machineId: number;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // elimina propiedades no definidas en el DTO
+      forbidNonWhitelisted: true, // lanza error si llegan propiedades extra
+      transform: true, // transforma tipos (string -> number)
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
