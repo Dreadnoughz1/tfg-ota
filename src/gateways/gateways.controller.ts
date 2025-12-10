0d37c5cf7a07706381963e3e07185e4bc62dc3d2
Joan Llop <p.desarrollo.ipla@gmail.com>
2025-12-10 08:55:19 +0100

    Módulo gateways terminado

--- src/gateways/gateways.controller.ts
+++ src/gateways/gateways.controller.ts
@@ -1,7 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { GatewaysService } from './gateways.service';
+import { SearchDto } from 'src/shared/dto';
+import { CreateGatewayDto, UpdateGatewayDto } from './dto';
 
 @Controller('gateways')
 export class GatewaysController {
   constructor(private readonly gatewaysService: GatewaysService) {}
+
+  @Post()
+  create(@Body() createGatewayDto: CreateGatewayDto) {
+    return this.gatewaysService.create(createGatewayDto);
+  }
+
+  @Get()
+  findAll(@Query() paginationDto: SearchDto) {
+    return this.gatewaysService.findAll(paginationDto);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.gatewaysService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateGatewayDto: UpdateGatewayDto) {
+    return this.gatewaysService.update(+id, updateGatewayDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.gatewaysService.remove(+id);
+  }
 }
