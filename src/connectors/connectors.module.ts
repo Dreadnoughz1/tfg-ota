ce5cec19ac4ff1279aba69732b32cb4a2ce51521
Joan Llop <p.desarrollo.ipla@gmail.com>
2025-12-10 12:36:11 +0100

    solucionado errores de módulos

--- src/connectors/connectors.module.ts
+++ src/connectors/connectors.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConnectorsService } from './connectors.service';
 import { ConnectorsController } from './connectors.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connector } from './entities/connector.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Connector])],
   controllers: [ConnectorsController],
   providers: [ConnectorsService],
 })
--- src/gateways/gateways.module.ts
+++ src/gateways/gateways.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GatewaysService } from './gateways.service';
 import { GatewaysController } from './gateways.controller';
+import { Gateway } from './entities/gateway.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Gateway])],
   controllers: [GatewaysController],
   providers: [GatewaysService],
 })
