6202aec816a2920e6477a819f9b44000d1c572a9
Joan Llop <p.desarrollo.ipla@gmail.com>
2026-01-12 08:10:19 +0100

    Eliminado frontend

--- frontend/src/auth/ProtectedRoute.tsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from 'react-router-dom';
-import { useAuth } from './AuthContext';
-import type { JSX } from 'react';
-
-export function ProtectedRoute({ children }: { children: JSX.Element }) {
-  const { isAuthenticated } = useAuth();
-
-  return isAuthenticated ? children : <Navigate to="/login" />;
-}
