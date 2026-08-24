78104cde6f79b1d9532de4e1ded9ba9f40d5e6ea
Dreadnoughz1 <al415730@uji.es>
2026-08-24 16:47:48 +0200

    class validators a los dto

--- src/gateways/dto/create-gateway.dto.ts
+++ src/gateways/dto/create-gateway.dto.ts
@@ -1,8 +1,19 @@
-import { Connector } from 'src/connectors/entities/connector.entity';
+import { IsArray, IsInt, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateGatewayDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
   location: string;
+
+  @IsArray()
+  @IsInt({ each: true })
   connectorsId: number[];
+
+  @IsArray()
+  @IsInt({ each: true })
   machinesId: number[];
 }
